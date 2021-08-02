d3086af937780af477e7468a79fe59da05dcaeec
mominahmed23 <mominahmad324@gmail.com>
2021-08-02 19:12:12 +0500

    todo list with edit functionality

--- src/components/Todo/index.jsx
+++ src/components/Todo/index.jsx
@@ -1,55 +1,83 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const Todo = () => {
-    const [showInput, setshowInput] = useState(false);
-    const [userInput, setUserInput] = useState('');
-    const [todoList, setTodoList] = useState([]);
-    const onchangeHandler = (e) => {
-        setUserInput(e.target.value);
+  const [showInput, setshowInput] = useState(false);
+  const [userInput, setUserInput] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const [editFlag, setEditFlag] = useState(-1);
+  const onchangeHandler = (e) => {
+    setUserInput(e.target.value);
+  };
+  const onsaveList = () => {
+    if (editFlag !== -1) {
+      let temp = [...todoList];
+      temp.splice(editFlag, 1, userInput);
+      setTodoList(temp);
+      setshowInput(false);
+      setUserInput("");
+      setEditFlag(-1);
+    } else {
+      let temp = [...todoList];
+      temp.push(userInput);
+      setTodoList(temp);
+      setshowInput(false);
+      setUserInput("");
     }
-    const onsaveList = () => {
-        let temp = [...todoList];
-        temp.push(userInput)
-        setTodoList(temp);
-        setshowInput(false);
-        setUserInput('');
-    }
-    const addTask=() =>{
-        setshowInput(true);
-    }
-    const onDelete= (index) =>{
-        let temp = [...todoList];
-        temp.splice(index,1);
-        setTodoList(temp);
-    }
-    useEffect(() => {
-        console.log("ran");
-    }, []);
-    return (
-        <div>
-            <div>
-                <button onClick={addTask}>add task</button>
-            </div>
-            {showInput && <div className="input-wrap">
-                <div>
-                    <input value={userInput} onChange={onchangeHandler} type="text" />
-                </div>
-                <div>
-                    <button onClick={()=>{setshowInput(false);setUserInput('');}}>cancel</button>
-                    <button onClick={onsaveList}>save</button>
-                </div>
-            
-            </div>}
-            {todoList.length ? (
-                   <div>
-                   <ul>
-                       {todoList.map((item, index) => <li key={item}>{item}<button onClick={()=>onDelete(index)}>del</button></li>)}
-                   </ul>
-               </div>
-            ) :(null)}
-         
+  };
+  const addTask = () => {
+    setshowInput(true);
+  };
+  const onDelete = (index) => {
+    let temp = [...todoList];
+    temp.splice(index, 1);
+    setTodoList(temp);
+  };
+  const onEdit = (index) => {
+    setEditFlag(index);
+    setshowInput(true);
+    setUserInput(todoList[index]);
+  };
+  useEffect(() => {
+    console.log("ran");
+  }, []);
+  return (
+    <div>
+      <div>
+        <button onClick={addTask}>add task</button>
+      </div>
+      {showInput && (
+        <div className="input-wrap">
+          <div>
+            <input value={userInput} onChange={onchangeHandler} type="text" />
+          </div>
+          <div>
+            <button
+              onClick={() => {
+                setshowInput(false);
+                setUserInput("");
+              }}
+            >
+              cancel
+            </button>
+            <button onClick={onsaveList}>save</button>
+          </div>
         </div>
-    )
-}
+      )}
+      {todoList.length ? (
+        <div>
+          <ul>
+            {todoList.map((item, index) => (
+              <li key={item}>
+                {item}
+                <button onClick={() => onDelete(index)}>del</button>
+                <button onClick={() => onEdit(index)}>edit</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : null}
+    </div>
+  );
+};
 
-export default Todo
+export default Todo;
